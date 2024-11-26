06ac454662a4c2bc50d48f4a3a72b6032f24b689
Austidav59 <adavis10@byui.edu>
2024-11-26 12:27:49 -0700

    war card game

---
+++ demo/demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Austin Davis on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
